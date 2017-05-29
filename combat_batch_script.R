9f522c51a58f4fd6f60c8a13d6408d4eb207950d
Edward Huang <ewhuang3@timan103.cs.illinois.edu>
2017-05-29 00:36:20 -0500

    Updated ComBat description in combat_batch_script.R

--- combat_batch_script.R
+++ combat_batch_script.R
@@ -1,12 +1,12 @@
+# Basic template for running ComBat.
 
-# Gene expression: edata
-# 22283x57 table:
-#         GSM71019.CEL    GSM71020.CEL
-# 1007_s_at   10.115170   8.628044
-# 1053_at 5.345168    5.063598
+# Variable edata: gene expression file
+# gene x sample:
+#               GSM71019.CEL    GSM71020.CEL
+# 1007_s_at     10.115170       8.628044
+# 1053_at       5.345168        5.063598
 
-
-# pheno:
+# Variable pheno: sample description file.
 #                 sample  outcome batch   cancer
 # GSM71019.CEL    1       Normal  3       Normal
 # GSM71020.CEL    2       Normal  2       Normal
@@ -24,4 +24,5 @@ edata = exprs(bladderEset)
 batch=pheno$batch
 modcombat = model.matrix(~1, data=pheno)
 modcancer = model.matrix(~cancer, data=pheno)
-combat_edata = ComBat(dat=edata, batch=batch, mod=modcombat, par.prior=TRUE, prior.plots=FALSE)
+combat_edata = ComBat(dat=edata, batch=batch, mod=modcombat, par.prior=TRUE,
+    prior.plots=FALSE)
